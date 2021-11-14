9af53c97f26be145c11c02dbdfaa33d8592e79aa
matttoledo <matt.toledo@poli.ufrj.br>
2021-11-14 14:30:16 -0300

    permitindo o metodo patch no cors e mudando o patch de create para post

--- src/main/java/com/verly/verlyservice/application/config/Configurations.java
+++ src/main/java/com/verly/verlyservice/application/config/Configurations.java
@@ -15,7 +15,7 @@ public class Configurations implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedMethods("GET", "POST", "PUT","PATCH","DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
 
