df7b8a30e63d4ea440ac77cc8e751ee955279649
Mike Andrade <mike.andrade.oliveira@gmail.com>
2022-07-18 20:41:28 -0300

    Criando user model

---
+++ src/main/java/com/verly/verlyservice/application/model/User.java
@@ -0,0 +1,67 @@
+package com.verly.verlyservice.application.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable, UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column(nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = true)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
